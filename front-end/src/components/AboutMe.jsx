9d71200d40bd8edb2126abd2310e9baa22185595
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-07-06 12:51:07 +1000

    test

--- front-end/src/components/AboutMe.jsx
+++ front-end/src/components/AboutMe.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CardImageVertical from './global-components/CardImageVertical';
 import aboutMe from '../datas/aboutMe';
 
+console.log(aboutMe.imgUrl);
 function AboutMe(props){
     return(
  
@@ -11,7 +12,8 @@ function AboutMe(props){
             
             <p className='section__subtitle section__subtitle--bgcolor about-me__subtitle shadow-2xl'>{aboutMe.subtitle}</p>
 
-            <CardImageVertical imgSrc={process.env.PUBLIC_URL+aboutMe.imgUrl} className="about-me__img" alt="more about me" loading="lazy"/>
+            <CardImageVertical imgSrc='/assets/images/Profile3.jpeg' className="about-me__img" alt="more about me" loading="lazy"/>
+            
             <div className="about-me__body">
                 <p>{aboutMe.content}</p>
                 
--- front-end/src/datas/aboutMe.js
+++ front-end/src/datas/aboutMe.js
@@ -6,7 +6,7 @@ const aboutMe =
     {
         title:"Who I am",
         subtitle:"Front End Developer based out of Sydney",
-        imgUrl:"assets/images/"+imageUrl,
+        imgUrl:"/assets/images/"+imageUrl,
         content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
       
       }
