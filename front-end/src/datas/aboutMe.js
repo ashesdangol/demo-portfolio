af009f43f628e62b83729adb9b6c5f98532eb8d1
Ashes Dangol <simachhetri@Simas-MacBook-Pro.local>
2022-07-14 03:49:59 +1000

    about me js fix name

--- front-end/src/datas/aboutMe.js
+++ front-end/src/datas/aboutMe.js
@@ -8,7 +8,7 @@ const aboutMe =
         subtitle:"Front End Developer based out of Sydney",
         highImageUrl:"/assets/images/"+imageName,
         mediumImageUrl:"/assets/medium-images/"+imageName,
-        smallImageUrl:"/assets/small-images/"+imageName,
+        lowImageUrl:"/assets/small-images/"+imageName,
         content:"UI/UX Front End Web Developer with ability to translate prototype, wireframe and mock-up designs into codes with several years of technical support experience. Proficient in HTML5, CSS3, JavaScript, libraries and frameworks such as Tailwind, Material UI, Bootstrap, Sass, jQuery to build responsive and interactive web sites including testing cross-browser compatibility. Experience in WordPress, Hyper Terminal, Node.js, Express, React.js, API, PHP Template, EJS Template, SQL, MongoDB, Git and Version control."
       
       }
